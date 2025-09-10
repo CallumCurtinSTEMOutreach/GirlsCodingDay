cfaa04ac04941b5a1004d740bf9007434175978c
CallumCurtinSTEMOutreach <callum.sullivan@curtin.edu.au>
2025-09-10 14:02:58 +0800

    Add files via upload

---
+++ story_game_skeleton.py
@@ -0,0 +1,20 @@
+intro_q = """Captain, we've been on the run from the space sherrifs for days now, the ship's logs are getting corrupted, please enter your name"""
+user_name = input(intro_q)
+situation_1 = "the first situation"
+print(situation_1)
+options_1 = ""
+answer_1 = input(options_1)
+if answer_1 == "1":
+    bad_end_1 = "you are dded :("
+    print(bad_end_1)
+if answer_1 == "2":
+    situation_1_2 = "the situation if they choose 2 first"
+    print(situation_1)
+    options_1_2 = "the options if the choose 2 first"
+    answer_2_1 = input(options_1_2)
+    if answer_2_1 == "1":
+        situation_2_1 = "the situation if they choose 2 first"
+        print(situation_2_1)
+        options_2_1 = "the options if the choose 2 first"
+        answer_2_1_1 = input(options_1_2)
+        
